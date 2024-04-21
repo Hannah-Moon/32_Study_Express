54d286d5924adc8fdd0e53681e7de1cdd1f19fc0
Hannah-Moon <59512737+Hannah-Moon@users.noreply.github.com>
2024-04-21 11:02:33 -0400

    more practice

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const logger = require('./middleware/logger')
+
 const members = require('./Members')
+const app = express();
 
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
-const logger = (req, res, next) => {
-    console.log('Hello');
-    next();
-}
 
 // Init middleware
 app.use(logger);
@@ -23,8 +20,15 @@ app.get('/api/members', (req, res) =>{
     res.json(members);
 });
 
+// Get Single Member
+app.get('/api/members/:id', (req, res) => {
+    res.setDefaultEncoding(requ.params.id);
+})
+
 
 // --------- [ Set static folder ]
 app.use(express.static(path.join(__dirname, 'public')))
 
+// --------- [ Local Port ]
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
